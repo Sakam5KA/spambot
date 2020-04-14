5e59bcc7dfb8ac721c041fa6a0f8e54c53d5ad44
Sakam5KA <63678175+Sakam5KA@users.noreply.github.com>
2020-04-14 23:39:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ user = 'sakam5ka@gmail.com'
 password = 'sakampetka123'
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
+server.ehlo()
 server.starttls()
 server.login(user, password)
 print("Login success")
 
 while True:
-    server.sendmail(user, 'nhcfizika@yahoo.com', 'Bonjour. Sakam 5ka!')
-    sleep(100)
+    try:
+        server.sendmail(user, 'nhcfizika@yahoo.com', 'Bonjour. Sakam 5ka!')
+        print("mail sent!")
+    except Exception as e:
+        sleep(100)
