74b4ed4fa56dfd13e213b3462705925d0167fc93
Sakam5KA <63678175+Sakam5KA@users.noreply.github.com>
2020-04-15 00:47:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,16 @@ from time import sleep
 user = 'sakam5ka@gmail.com'
 password = 'sakampetka123'
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.ehlo()
-server.starttls()
-server.login(user, password)
-print("Login success")
-
 while True:
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login(user, password)
+    print("Login success")
     server.sendmail(user, 'nhcfizika@yahoo.com', 'Bonjour. Sakam 5ka!')
     print("mail sent!")
-    s.quit()
-    sleep(60)
+    server.quit()
+    sleep(1)
     
     
     #    try:
